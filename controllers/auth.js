d2220a7f3aa94d610d3b917654e463c1502a2c61
ian maulen <ianmaulen@gmail.com>
2023-02-23 15:36:03 -0300

    Google sign in a RESTServer

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,8 +1,9 @@
-const { response } = require("express");
+const { response, request } = require("express");
 const bcryptjs = require("bcryptjs");
 
 const Usuario = require("../models/user");
 const generarJWT = require("../helpers/generarJWT");
+const { googleVerify } = require("../helpers/google-verify");
 
 const login = async(req, res = response) => {
 
@@ -53,6 +54,53 @@ const login = async(req, res = response) => {
 
 }
 
+const googleSignIn = async( req, res = response ) => {
+  const { id_token } = req.body;
+
+  try {
+    const {correo, img, nombre} = await googleVerify(id_token);
+
+    let usuario = await Usuario.findOne({correo});
+
+    if (!usuario) {
+      const rol = 'USER_ROLE'
+      usuario = new Usuario({
+        nombre, 
+        correo, 
+        password: ':P', 
+        rol,
+        img,
+        google: true
+      });
+      await usuario.save();
+    }
+
+    if (!usuario.estado) {
+      return res.status(401).json({
+        msg: 'Hable con el administrador, usuario bloqueado'
+      })
+    }
+
+    const token = await generarJWT( usuario.id );
+
+
+    res.json({
+      msg: 'Todo OK',
+      correo, 
+      img,
+      nombre,
+      usuario,
+      token
+    })
+  } catch (error) {
+    res.status(400).json({
+      ok: false,
+      msg: 'El token no se pudo verificar'
+    })
+  }
+}
+
 module.exports = {
-  login
+  login,
+  googleSignIn
 }
